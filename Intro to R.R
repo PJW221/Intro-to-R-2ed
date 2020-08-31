60374afe55aefafb2dd9b08bf5571fdec4f88878
PJ Walsh <pjw221@lehigh.edu>
2020-08-31 14:02:59 -0400

    edited message

--- Intro to R.R
+++ Intro to R.R
@@ -1,7 +1,7 @@
 # R script to accompany Intro to R
 # Copy for PJ Walsh 2020
 ######################################################
-
+# Paddington Learns R
 # You will see that you can put non-executable "comment" lines
 # in your script by putting "#" at the beginning of the lines.
 # 
